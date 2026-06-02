6e0d5a882b469fcbdb0e918dc182617ac0295baa
Julio Reyes <julio.reyes@my.wheaton.edu>
2026-06-02 10:30:14 -0600

    solution and test files to problem 217

--- arrhash_probs/sol_217_containsDuplicate.py
+++ arrhash_probs/sol_217_containsDuplicate.py
@@ -1,14 +1,20 @@
 class Solution:
 	def containsDuplicate(self, nums: List[int]) -> bool:
+		#array is empty
 		if not nums:
 			return False
 
+		#store seen numbers for comparison
 		seen = set()
 
+		#iterate through give nums list and add to set
 		for num in nums:
+			#list contains duplicate
 			if num in seen:
 				return True
+			#add to set
 			seen.add(num)
 
+		#no duplicate was found
 		return False
 	
